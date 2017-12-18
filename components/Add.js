f4f5c84d3366b43b315d78ab43056ade96e361e3
Yannick <yannick1691@users.noreply.github.com>
2017-12-18 09:27:26 +0100

    Base of Add, Fixed typo

--- components/Add.js
+++ components/Add.js
@@ -1,16 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+
+// Import React native Form
+// import { Form, InputField, LinkField, SwitchField, PickerField,DatePickerField, TimePickerField } from 'react-native-form-generator';
 
 // Components
 import Header from './Header';
 
 export default class Add extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.success = this.success.bind(this);
+    this.error = this.error.bind(this);
+
+    this.state = {
+      longitude: '',
+      latitude: '',
+
+    }
+  }
+
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition(this.success, this.error);
+  }
+
+  success(pos) {
+    var crd = pos.coords;
+
+    this.setState({
+      latitude: `${crd.latitude}`,
+      longitude: `${crd.longitude}`,
+      date: ''
+    });
+  };
+
+  error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  };
+
+
   render() {
+    const date = new Date();
     return (
       <View style={styles.container}>
         <Header head={this.props.head}/>
         <View style={styles.main}>
-          <Text>Add</Text>
+          <View>
+            <TextInput
+              onChangeText={(longitude) => this.setState({ longitude })}
+              value={this.state.longitude}
+              style={styles.input}
+              autoCorrect={false}
+            />
+            <TextInput
+              onChangeText={(latitude) => this.setState({ latitude })}
+              value={this.state.latitude}
+              style={styles.input}
+              autoCorrect={false}
+            />
+            <TextInput
+              onChangeText={(date) => this.setState({ date })}
+              value={`${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`}
+              style={styles.input}
+              autoCorrect={false}
+            />
+          </View>
         </View>
       </View>
     );
@@ -26,5 +81,22 @@ const styles = StyleSheet.create({
   },
   main: {
     flex: 1,
+    width: '100%',
+  },
+  label: {
+    fontWeight: '800',
+    fontSize: 15,
+    color: '#333',
+    marginTop: 5,
+  },
+  input: {
+    borderColor: '#333',
+    borderStyle: 'solid',
+    borderWidth: 0.5,
+    height: 35,
+    width: '100%',
+    marginTop: 5,
+    marginBottom: 5,
+    padding: 5,
   },
 });
--- components/Measurement.js
+++ components/Measurement.js
@@ -33,14 +33,14 @@ export default class Measurement extends React.Component {
       <View style={styles.container}>
         <Header head={this.props.head}/>
         <ScrollView style={styles.main}>
-          {measurement.longtitude &&
+          {measurement.longitude &&
             <View style={styles.row}>
-              <Text style={styles.label}>Longtitude</Text>
-              <Text style={styles.value}>{measurement.longtitude}</Text>
+              <Text style={styles.label}>Longitude</Text>
+              <Text style={styles.value}>{measurement.longitude}</Text>
             </View>
           }
           {measurement.latitude &&
-            <View style={styles.row}>
+            <View style={styles.rowodd}>
               <Text style={styles.label}>Latitude</Text>
               <Text style={styles.value}>{measurement.latitude}</Text>
             </View>
@@ -52,7 +52,7 @@ export default class Measurement extends React.Component {
             </View>
           }
           {measurement.acidity &&
-            <View style={styles.row}>
+            <View style={styles.rowodd}>
               <Text style={styles.label}>Acidity (pH)</Text>
               <Text style={styles.value}>{measurement.acidity}</Text>
             </View>
@@ -86,17 +86,25 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '100%',
+    backgroundColor: '#fafafa',
+    padding: 10,
+  },
+  rowodd: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
     padding: 10,
   },
   value: {
     fontWeight: '300',
     fontSize: 14,
     width: '50%',
-    color: '#333',
+    color: '#444',
   },
   label: {
     fontWeight: '900',
     fontSize: 15,
-    color: '#333',
+    color: '#555',
   }
 });
